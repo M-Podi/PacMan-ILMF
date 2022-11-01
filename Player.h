f1a5c4970e05a4cfad8d7acd4b9f3b737e3fa3d1
M-Podi <mateipodeanu18@gmail.com>
2022-11-01 04:28:00 +0200

    Prima tema

---
+++ Player.h
@@ -0,0 +1,25 @@
+#include<iostream>
+
+class Player {
+private:
+    std::string name;
+    int bestScore;
+    time_t timePlayed;
+public:
+    //Player();
+    Player(std::string name="Nume", int score = 0, time_t time = 0);
+    Player(const Player& player);
+    ~Player();
+    void setBestScore(int score);
+    int getBestScore() const;
+
+    void setName(const std::string& name);
+    std::string getName() const;
+
+    void setTime(time_t time);
+    time_t getTime() const;
+
+    friend std::ostream& operator << (std::ostream& os, const Player& other);
+    Player& operator = (const Player& other) = default;
+};
+std::ostream& operator<<(std::ostream& os, Player& player1);
