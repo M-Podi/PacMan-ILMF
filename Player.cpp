293b52532d21faf9c35d0f954d7bef718ec3e7f2
M-Podi <mateipodeanu18@gmail.com>
2022-11-02 01:04:25 +0200

    .

--- Player.cpp
+++ Player.cpp
@@ -1,31 +1,38 @@
 #include "Player.h"
 
-Player::Player(std::string name, int scor, time_t time): name(name), bestScore(scor), timePlayed(time) {}
+Player::Player(const std::string &name, int scor, time_t time) : name(name), bestScore(scor), timePlayed(time) {}
 
 Player::~Player() {
     std::cout << "Am apelat deconstructorul \n";
 }
-Player::Player(const Player& other): name(other.name), bestScore(other.bestScore), timePlayed(other.timePlayed) { }
+
+Player::Player(const Player &other) : name(other.name), bestScore(other.bestScore), timePlayed(other.timePlayed) {}
 
 [[maybe_unused]] void Player::setBestScore(int score) {
     this->bestScore = score;
 }
+
 int Player::getBestScore() const {
     return bestScore;
 }
-[[maybe_unused]] void Player::setName(const std::string& pName) {
+
+[[maybe_unused]] void Player::setName(const std::string &pName) {
     this->name = pName;
 }
+
 std::string Player::getName() const {
     return name;
 }
+
 [[maybe_unused]] void Player::setTime(time_t time) {
     this->timePlayed = time;
 }
+
 time_t Player::getTime() const {
     return timePlayed;
 }
-std::ostream& operator<<(std::ostream& os,const Player& other) {
+
+std::ostream &operator<<(std::ostream &os, const Player &other) {
     os << other.getName() << " a obtinut " << other.getBestScore() << " puncte in " << other.getTime() << " secunde\n";
     return os;
 }
--- Player.h
+++ Player.h
@@ -7,19 +7,27 @@ private:
     time_t timePlayed;
 public:
     //Player();
-    Player(std::string name="Nume", int score = 0, time_t time = 0);
-    Player(const Player& player);
+    Player(const std::string &name = "Nume", int score = 0, time_t time = 0);
+
+    Player(const Player &player);
+
     ~Player();
+
     void setBestScore(int score);
+
     int getBestScore() const;
 
-    void setName(const std::string& name);
+    void setName(const std::string &name);
+
     std::string getName() const;
 
     void setTime(time_t time);
+
     time_t getTime() const;
 
-    friend std::ostream& operator << (std::ostream& os, const Player& other);
-    Player& operator = (const Player& other) = default;
+    friend std::ostream &operator<<(std::ostream &os, const Player &other);
+
+    Player &operator=(const Player &other) = default;
 };
-std::ostream& operator<<(std::ostream& os,const Player& player1);
+
+std::ostream &operator<<(std::ostream &os, const Player &player1);
