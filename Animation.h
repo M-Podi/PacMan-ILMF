ec4b66409940754b2efdc75afeb188f4bb923f5c
M-Podi <mateipodeanu18@gmail.com>
2022-11-08 03:53:06 +0200

    animated menu background

---
+++ Animation.h
@@ -0,0 +1,24 @@
+//
+// Created by Matei on 11/7/2022.
+//
+
+#ifndef OOP_ANIMATION_H
+#define OOP_ANIMATION_H
+
+#include"SFML/Graphics.hpp"
+
+class Animation {
+public:
+    Animation(sf::Texture* texture, sf::Vector2u imageCount,float switchTime);
+    ~Animation();
+    void Update(int row,float deltaTime);
+    sf::IntRect uvRect;
+private:
+    sf::Vector2u imageCount;
+    sf::Vector2u currentImage;
+    float totalTime;
+    float switchTime;
+};
+
+
+#endif //OOP_ANIMATION_H
